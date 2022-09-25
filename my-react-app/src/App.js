71ea33876f670231b8cef721871bc2744b6042e2
viviana rodriguez <vivianaprodriguez@live.com>
2022-09-25 00:35:45 -0400

    made an about me section

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+
+function App() {
+  return (
+    <div className="my-5">
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
