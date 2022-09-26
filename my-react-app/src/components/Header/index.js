4d16abad38a066c3d9b1f7821bc6f5a1682419ad
viviana rodriguez <vivianaprodriguez@live.com>
2022-09-25 20:48:56 -0400

    added all folders

---
+++ my-react-app/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import Nav from '../components/Nav';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Portfolio from '../components/Portfolio';
+import Resume from '../components/Resume';
+import React, {Component} from 'react';
+
+function Header() {
+  
+    return (
+        
+    )
+}
--- my-react-app/src/components/Nav/index.js
+++ my-react-app/src/components/Nav/index.js
@@ -5,39 +5,39 @@ function Nav() {
 
     return (
         <div className='topnav'>
-            <div className='topnav-left'>
-                <NavLink
-                    activeClassName="navbar__link--active"
-                    className="navbar__link"
-                    to="/about"
-                >
-                    About
-                </NavLink>
-            </div>
-            <div className='topnav-right'>
-                <NavLink
-                    activeClassName="navbar__link--active"
-                    className="navbar__link"
-                    to="/portfolio"
-                >
-                    Portfolio
-                </NavLink>
-                <NavLink
-                    activeClassName="navbar__link--active"
-                    className="navbar__link"
-                    to="/contact"
-                >
-                    Contact
-                </NavLink>
-                <NavLink
-                    activeClassName="navbar__link--active"
-                    className="navbar__link"
-                    to="/resume"
-                >
-                    Resume
-                </NavLink>
-            </div>
+        <div className='topnav-left'>
+            <NavLink
+                activeClassName="navbar__link--active"
+                className="navbar__link"
+                to="/about"
+            >
+                About
+            </NavLink>
         </div>
+        <div className='topnav-right'>
+            <NavLink
+                activeClassName="navbar__link--active"
+                className="navbar__link"
+                to="/portfolio"
+            >
+                Portfolio
+            </NavLink>
+            <NavLink
+                activeClassName="navbar__link--active"
+                className="navbar__link"
+                to="/contact"
+            >
+                Contact
+            </NavLink>
+            <NavLink
+                activeClassName="navbar__link--active"
+                className="navbar__link"
+                to="/resume"
+            >
+                Resume
+            </NavLink>
+        </div>
+    </div>
     );
 }
 
