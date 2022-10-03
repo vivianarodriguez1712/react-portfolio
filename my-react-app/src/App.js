3823db3182d0b88ee0e95855b13a33c4275d0bec
viviana rodriguez <vivianaprodriguez@live.com>
2022-10-02 20:06:38 -0400

    changed code to make changes to images

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 import React from 'react';
 import About from './components/About';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import codequiz from "./components/assets/images/codequiz.png";
-import "./components/assets/css/style.css";
+// import codequiz from "./components/assets/images/codequiz.png";
+// import "./components/assets/css/style.css";
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
       <main>
         <About></About>
         <Portfolio>
-        <img className="codequiz" src={"./components/assets/images/codequiz.png"} />
+        <img src={process.env.PUBLIC_URL + '/assets/images/codequiz.png'} />         
         </Portfolio>
         <Contact/>
       </main>
--- my-react-app/src/components/Portfolio/index.js
+++ my-react-app/src/components/Portfolio/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import runbuddy from '../assets/images/runbuddy.png'
-import moviesearchengine from '../assets/images/moviesearchengine.png'
-import workdayscheduler from '../assets/images/workdayscheduler.png'
-import weatherdashboard from '../assets/images/weatherdashboard.png'
-import codequiz from '../assets/images/codequiz.png'
-import portfolio from '../assets/images/portfolio.png'
+// import runbuddy from '../assets/images/runbuddy.png'
+// import moviesearchengine from './assets/images/moviesearchengine.png'
+// import workdayscheduler from './assets/images/workdayscheduler.png'
+// import weatherdashboard from './assets/images/weatherdashboard.png'
+// import codequiz from './assets/images/codequiz.png'
+// import portfolio from './assets/images/portfolio.png'
 
 function Portfolio() {
     const contents = [
@@ -12,52 +12,53 @@ function Portfolio() {
             name: 'Run Buddy',
             link: 'https://lernantino.github.io/run-buddy/',
             Glink: 'https://github.com/vivianarodriguez1712/run-buddy',
-            image: runbuddy
+            image: 'runbuddy'
         },
         {
             name: 'Movie Search Engine',
             link: ' https://rrvallar.github.io/Movie-Search-Engine/',
             Glink: 'https://github.com/vivianarodriguez1712/Movie-Search-Engine',
-            image: moviesearchengine
+            image: 'moviesearchengine'
         },
         {
             name: 'Work Day Scheduler',
             link: ' https://vivianarodriguez1712.github.io/work-day-scheduler/',
             Glink: 'https://github.com/vivianarodriguez1712/work-day-scheduler',
-            image: workdayscheduler
+            image: 'workdayscheduler'
         },
         {
             name: 'Weather Dashboard',
             link: ' https://vivianarodriguez1712.github.io/weather-dashboard/',
             Glink: 'https://github.com/vivianarodriguez1712/weather-dashboard',
-            image: weatherdashboard
+            image: 'weatherdashboard'
         },
         {
             name: 'Code Quiz',
             link: ' https://vivianarodriguez1712.github.io/code-quiz/',
             Glink: 'https://github.com/vivianarodriguez1712/code-quiz',
-            image: codequiz
+            image: 'codequiz'
         },
         {
             name: 'Portfolio',
             link: ' https://vivianarodriguez1712.github.io/Portfolio-/',
             Glink: 'https://github.com/vivianarodriguez1712/Portfolio-',
-            image: portfolio
+            image: 'portfolio'
         },
 
     ]
     return (
-        <>
+        <section class="apps-developed">
             <h2 id='portfolio'>Portfolio</h2>
             <ul>
                 {contents.map((content) =>
                     <li key={content.name}>
                         <a href={content.link}>{content.name}</a>
                         <a href={content.Glink}> Repository</a>
-                        <img src={content.image} alt={content.name}></img>
+                        <img src={process.env.PUBLIC_URL + '/assets/images/' + content.image +'.png'} />
+                        {/* <img src={content.image} alt={content.name}></img> */}
                     </li>)}
             </ul>
-        </>
+        </section>
     );
 }
 
