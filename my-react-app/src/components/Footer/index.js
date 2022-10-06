171a2783251eaf66d94804ba1620abbbcbce2332
viviana rodriguez <vivianaprodriguez@live.com>
2022-10-05 22:49:06 -0400

    changed the links in footer

--- my-react-app/src/components/Footer/index.js
+++ my-react-app/src/components/Footer/index.js
@@ -4,10 +4,10 @@ export default function Footer() {
 
     return (
         <div class='footer' id='footer'>
-        <a href='https://github.com/vivianarodriguez1712' alt='Github' class='icon'></a>
-        <a href='www.linkedin.com/in/viviana-rodriguez-303301214' alt='Linkedin Profile' class='icon'></a>
-        <a href='https://stackoverflow.com/users/18509993/viviana-rodriguez' alt='Stackoverflow Link' class='icon'></a>
-            <p>
+        <a href='https://github.com/vivianarodriguez1712' class='icon'>Github</a>
+        <a href='www.linkedin.com/in/viviana-rodriguez-303301214' class='icon'>Linkedin Profile</a>
+        <a href='https://stackoverflow.com/users/18509993/viviana-rodriguez' class='icon'>Stackoverflow link</a>
+               <p>
                 Viviana Rodriguez 
             </p>
         </div>
